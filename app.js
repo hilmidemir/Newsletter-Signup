15b4cfbd07a7050183e93a3f07306decfba2c500
Hilmi Demir <hilmidemirbjk@gmail.com>
2022-06-18 11:13:10 +0300

    some updates

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const https = require("node:https");
 const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  var mail = req.body.fname;
+  var mail = req.body.lname;
+  var mail = req.body.email;
+  console.log(mail);
+});
+
 app.listen(port, function() {
   console.log("Port listening 3000");
-})
+});
